40d2c7c8a104b9a614bdf189b63791f58263bfaf
Jose Luis Reyes <38513131+jose161015@users.noreply.github.com>
2022-01-19 14:29:51 -0600

    jose luis
    requerimientos

--- Reface_1/settings.py
+++ Reface_1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-hk%tpr$d++1u*rgukkgnd4$6z^m7ms+=$1#l&c-hv9u^n*noqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35 .239.58.219']
 
 
 # Application definition
